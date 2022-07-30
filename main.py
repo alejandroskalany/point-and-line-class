7ce083fd29f51561407a6bafcbf2ad38914d6b5a
Alejandro Skalany <alskalany@gmail.com>
2022-07-30 13:31:48 -0500

    first commit, point class works

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.printer = ("(" + str(x) + "," + str(y) + ")")
 
-    def __init__(self, x1, x2, y1, y2)
-        self.x1 = x1
-        self.x2 = x2
-        self.y1 = y1
-        self.y2 = y2
+    def __str__(self):
+        return str(self.printer)
 
-class Line:
+a_point = Point(1,2)
+another_point = Point(2,3)
 
 
+#class Line:
 
+
+
+
+
+if __name__ == "__main__":
+
+    print(a_point)
+    print(another_point)
